d4be1db8c946a38d8e73f676e6402df477312d2d
mwbrennan <mwb5@me.com>
2021-04-12 16:08:06 -0500

    mvp

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -12,14 +12,18 @@ const getById = id => {
 
 const create = account => {
   // DO YOUR MAGIC
+  return db('accounts').insert(account)
 }
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
+  return db('accounts').where('id', id)
+  .update(account);
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  return db('accounts').where('id', id).del();
 }
 
 module.exports = {
